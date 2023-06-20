895d9f01f9d1657b430d354d8e485173db2f9a8c
kotobukid <kotobuki@daqua.jp>
2023-06-20 17:45:11 +0900

    esmoduleでexpressをテスト

---
+++ esmodule_sample/app.test.js
@@ -0,0 +1,11 @@
+import request from "supertest-as-promised";
+
+import {app} from './app';
+
+describe('express rendering', () => {
+    it('GET / displays hello world', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('hello world');
+    });
+});
