40906e05e2c33178fbaafa15d9f881a5f2526906
kotobukid <kotobuki@daqua.jp>
2023-06-21 12:41:12 +0900

    テストをtsで記述はしたいがignoreの使用は避けたいということで、現時点でカスタムマッチャーの登録を行えなかったため代替のマッチャーを使用する

--- esmodule_sample/app.test.ts
+++ esmodule_sample/app.test.ts
@@ -2,23 +2,6 @@ import request from "supertest";
 import {describe, it, expect} from "@jest/globals";
 import {app} from './app';
 
-declare global {
-    namespace jest {
-        interface Matchers<R> {
-            toBeNumeric(): R;
-        }
-    }
-}
-
-expect.extend({
-    toBeNumeric(value) {
-        const pass = !isNaN(parseFloat(value)) && isFinite(value);
-        return {
-            pass,
-            message: () => pass ? `Expected ${value} not to be numeric` : `Expected ${value} to be numeric`,
-        };
-    },
-});
 
 describe('express rendering', () => {
     it('GET / displays hello world', async () => {
@@ -30,14 +13,12 @@ describe('express rendering', () => {
     it('GET /count displays any number', async () => {
         const response = await request(app).get('/count');
         expect(response.statusCode).toBe(200);
-        // @ts-ignore
-        expect(response.text).toBeNumeric();
+        expect(response.text).toMatch(/[0-9]+/);
     });
 
     it('GET /count?reset=yes resets number to 0', async () => {
         const response = await request(app).get('/count?reset=yes');
         expect(response.statusCode).toBe(200);
-        // @ts-ignore
         expect(response.text).toBe('0');
 
     });
