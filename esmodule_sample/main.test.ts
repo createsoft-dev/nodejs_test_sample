3d27ba1b91739bd0b0eb4c51d215831600e9c80a
kotobukid <kotobuki@daqua.jp>
2023-06-21 14:15:32 +0900

    もう一つのテストファイルをtsに

--- esmodule_sample/main.test.ts
+++ esmodule_sample/main.test.ts
@@ -1,11 +1,12 @@
+import {describe, it, expect} from "@jest/globals";
 import {add, add_async} from "./func.js";
 
-describe('math', () => {
-    it('add', () => {
+describe('math', (): void => {
+    it('add', (): void => {
         expect(add(1, 2)).toBe(3);
     });
 
-    it('add async', () => {
+    it('add async', (): void => {
         expect(add_async(1, 2)).resolves.toBe(3);
     });
 });
