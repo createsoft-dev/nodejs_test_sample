d816b0718856a58b9afad979f2caf2ad67310fa3
kotobukid <kotobuki@daqua.jp>
2023-06-21 12:09:21 +0900

    テスト終了後にredisへの接続を明示的に終了(Redis::quit)するようにしないと、テスト完了後もプロセスが残り続ける

--- esmodule_sample/app.test.ts
+++ esmodule_sample/app.test.ts
@@ -2,10 +2,51 @@ import request from "supertest";
 import {describe, it, expect} from "@jest/globals";
 import {app} from './app';
 
+declare global {
+    namespace jest {
+        interface Matchers<R> {
+            toBeNumeric(): R;
+        }
+    }
+}
+
+expect.extend({
+    toBeNumeric(value) {
+        const pass = !isNaN(parseFloat(value)) && isFinite(value);
+        return {
+            pass,
+            message: () => pass ? `Expected ${value} not to be numeric` : `Expected ${value} to be numeric`,
+        };
+    },
+});
+
 describe('express rendering', () => {
     it('GET / displays hello world', async () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe('hello world');
+    });
+
+    it('GET /count displays any number', async () => {
+        const response = await request(app).get('/count');
+        expect(response.statusCode).toBe(200);
+        // @ts-ignore
+        expect(response.text).toBeNumeric();
+    });
+
+    it('GET /count?reset=yes resets number to 0', async () => {
+        const response = await request(app).get('/count?reset=yes');
+        expect(response.statusCode).toBe(200);
+        // @ts-ignore
+        expect(response.text).toBe('0');
+
+    });
+
+    it('GET /count after reset displays 1', async () => {
+        const response = await request(app).get('/count');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('1');
+
+        app.locals.settings.redis.quit();
     });
 });
--- esmodule_sample/app.ts
+++ esmodule_sample/app.ts
@@ -1,11 +1,30 @@
 import express, {Router, type Request, type Response} from 'express';
+import Redis from 'ioredis';
 
 const app: express.Application = express();
 
 const router: Router = Router();
 
+const redis: Redis = new Redis();
+app.set('redis', redis);
+
 router.get('/', (req: Request, res: Response): void => {
     res.send('hello world');
+});
+
+router.get('/count', (req: Request<any, any, { reset?: 'yes' }>, res: Response<string>): void => {
+
+    const redis = req.app.locals.settings.redis;
+
+    if (req.query.reset === 'yes') {
+        redis.set('count', 0).then((): void => {
+            res.send(`${0}`);
+        });
+    } else {
+        redis.incr('count').then((count: number): void => {
+            res.send(`${count}`);
+        });
+    }
 });
 
 app.use(router);
